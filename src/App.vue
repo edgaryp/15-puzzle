50fa940e9444b657d026e9642b2e52d7a2cbf96f
Edgar Pan <edgaryp@me.com>
2019-01-26 20:12:58 +1100

    Added puzzle logic

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,27 @@
 <template>
   <div id="app">
-    <Puzzle />
+    <div class="container">
+      <div class="row">
+        <div class="col-sm-10 offset-sm-1">
+          <h1>15 Puzzle</h1>
+          <div class="container">
+            <div class="row">
+              <Puzzle />
+            </div>
+            <div v-if="invalidMoveTile"  class="row">
+              <AlertMessage />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 import { mapState, mapGetters, mapActions } from 'vuex';
 import Puzzle from './components/Puzzle.vue';
+import AlertMessage from './components/AlertMessage.vue';
 
 export default {
   name: 'app',
@@ -16,29 +31,47 @@ export default {
     };
   },
   components: {
-    Puzzle
+    Puzzle,
+    AlertMessage
   },
   computed: {
     ...mapState([
-      'puzzleDimension'
+      'puzzleDimension',
+      'tiles',
+      'targetTile'
     ]),
     ...mapGetters([
       'totalTilesCount',
-      'tiles',
-      'shuffleTilesInGrid'
+      'shuffleTilesInGrid',
+      'invalidMoveTile'
     ])
   },
   methods: {
     ...mapActions([
-
+      'createTiles'
     ])
   },
   created() {
-
+    this.createTiles();
   }
 };
 </script>
 
 <style lang="less">
 
+#app {
+  > .container {
+    padding-top: 3%;
+  }
+}
+
+h1 {
+  text-align: center;
+}
+
+@media (max-width: 575.98px) {
+  h1 {
+    font-size: 1.8em !important;
+  }
+}
 </style>
---
+++ src/components/AlertMessage.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="alert alert-primary col-12" role="alert">
+    This is a primary alert—check it out!
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'alert-message'
+};
+</script>
