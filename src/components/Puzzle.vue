f40ef44ee2df395c827645f0080f2c6d71d402dc
Edgar Pan <edgaryp@me.com>
2019-01-26 20:12:58 +1100

    Added puzzle logic

--- src/components/Puzzle.vue
+++ src/components/Puzzle.vue
@@ -1,35 +1,129 @@
 <template>
-  <table>
-    <tr v-for="(row, index) in shuffleTilesInGrid" :key="index">
-      <td v-for="(tile, index) in row" :key="index">
-        {{tile || ''}}
-      </td>
-    </tr>
-  </table>
+  <div class="puzzle-wrap col-12">
+    <div v-if="!gameStart" class="overlay">
+      <button type="button" class="btn btn-success btn-lg" @click="start(3)">Easy</button>
+      <button type="button" class="btn btn-success btn-lg" @click="start(4)">Medium</button>
+      <button type="button" class="btn btn-success btn-lg" @click="start(5)">Hard</button>
+    </div>
+    <div :class="`row row-${index}`" v-for="(row, index) in shuffleTilesInGrid" :key="index">
+      <div class="tile" :class="{'empty': tile === 0}" v-for="(tile, index) in row" :key="index" :style="{ width: 100 / puzzleDimension + '%', paddingTop: 100 / puzzleDimension + '%' }" @click="tileClick(tile)">
+        <div>
+          {{tile || ''}}
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-import { mapState, mapGetters } from 'vuex';
+import { mapState, mapGetters, mapMutations, mapActions } from 'vuex';
 
 export default {
   name: 'puzzle',
   data() {
     return {
-
+      gameStart: false
     };
   },
   computed: {
     ...mapState([
-      'puzzleDimension'
+      'puzzleDimension',
+      'tiles',
+      'targetTile'
     ]),
     ...mapGetters([
       'totalTilesCount',
-      'tiles',
-      'shuffleTilesInGrid'
+      'shuffleTilesInGrid',
+      'targetTileNeighbors',
+      'invalidMoveTile'
     ])
+  },
+  methods: {
+    ...mapMutations([
+      'setTargetTile',
+      'setRandomiseTiles'
+    ]),
+    ...mapActions([
+      'updateTiles',
+      'createTiles'
+    ]),
+    start(difficulty) {
+      this.createTiles(difficulty);
+      this.gameStart = !this.gameStart;
+    },
+    tileClick(tile) {
+      if (tile) {
+        this.setTargetTile(tile);
+        this.updateTiles(tile);
+      }
+    }
   }
 };
 </script>
 
 <style lang="less" scoped>
+.puzzle-wrap {
+  position: relative;
+  margin-top: 20px;
+  margin-bottom: 20px;
+  .overlay {
+    width: 100%;
+    height: 100%;
+    background-color: rgba(255, 255, 255, 0.6);
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    position: absolute;
+    top: 0;
+    left: 0;
+    z-index: 10;
+    .btn {
+      margin: 10px;
+      @media (max-width: 575.98px) {
+        font-size: 1em !important;
+      }
+    }
+  }
+  .row {
+    display: flex;
+    justify-content: space-between;
+  }
+  .tile {
+    position: relative;
+    div {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      position: absolute;
+      top: 0;
+      left: 0;
+      width: 100%;
+      height: 100%;
+      background-color: #f74902;
+      border: 2px solid white;
+      color: white;
+      font-size: 2.5em;
+      @media (max-width: 575.98px) {
+        font-size: 1.7em;
+      }
+    }
+    &.empty div {
+      background-color: white;
+    }
+    &:not(.empty) {
+      cursor: pointer;
+    }
+  }
+  .label {
+    color: white;
+    width: 100%;
+    height: 100%;
+    vertical-align: middle;
+    text-align: center;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    padding: 102px;
+  }
+}
 </style>
--- src/store.js
+++ src/store.js
@@ -1,4 +1,4 @@
-// /* eslint-disable */
+/* eslint-disable */
 import Vue from 'vue';
 import Vuex from 'vuex';
 
@@ -6,32 +6,94 @@ Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
-    puzzleDimension: 4
+    puzzleDimension: 4,
+    tiles: null,
+    targetTile: {
+      label: null,
+      index: null
+    }
   },
   getters: {
     totalTilesCount(state) {
       return state.puzzleDimension ** 2;
     },
-    tiles(state, getters) {
-      const tiles = [];
-      for (let i = 0; i <= getters.totalTilesCount; i += 1) {
-        tiles.push(i);
+    shuffleTilesInGrid(state) {
+      const tiles = [...state.tiles];
+      const shuffleTilesInGrid = [];
+      for (let i = 0; i < state.puzzleDimension; i += 1) {
+        shuffleTilesInGrid.push(tiles.splice(0, state.puzzleDimension));
       }
-      return tiles.sort(() => { return Math.random() - 0.5; });
+      return shuffleTilesInGrid;
     },
-    shuffleTilesInGrid(state, getters) {
-      const tilesInGrid = [];
-      const tiles = [...getters.tiles];
-      for (let j = 0; j < state.puzzleDimension; j += 1) {
-        tilesInGrid.push(tiles.splice(0, state.puzzleDimension));
+    targetTileNeighbors(state) {
+      const { index } = state.targetTile;
+      const { puzzleDimension } = state;
+      if (state.targetTile.label) {
+        return {
+          self: state.tiles[index],
+          top: state.tiles[index - puzzleDimension] === 0 ? 'empty' : null,
+          right: index % puzzleDimension !== 3 ? state.tiles[index + 1] || 'empty' : null,
+          bottom: state.tiles[index + puzzleDimension] === 0 ? 'empty' : null,
+          left: index % puzzleDimension ? state.tiles[index - 1] || 'empty' : null
+        };
+      } else {
+        return null;
       }
-      return tilesInGrid;
+    },
+    invalidMoveTile(state, getters) {
+      return (getters.targetTileNeighbors && getters.targetTileNeighbors.self !== 0) && true;
     }
   },
   mutations: {
-
+    setRandomiseTiles(state, difficulty) {
+      state.puzzleDimension = difficulty;
+    },
+    setTiles(state, tiles) {
+      state.tiles = [...tiles];
+    },
+    setTargetTile(state, tile) {
+      state.targetTile.label = tile;
+      state.targetTile.index = state.tiles.indexOf(tile);
+    },
+    setUpdateTiles(state, data) {
+      const { self, top, right, bottom, left } = data.targetTileNeighbors;
+      const { index, label } = state.targetTile;
+      const tiles = [...state.tiles];
+      if (label > 0) {
+        if (top === 'empty') {
+          tiles[index - state.puzzleDimension] = label;
+          tiles[index] = 0;
+        } else if (right === 'empty') {
+          tiles[index + 1] = label;
+          tiles[index] = 0;
+        } else if (bottom === 'empty') {
+          tiles[index + state.puzzleDimension] = label;
+          tiles[index] = 0;
+        } else if (left === 'empty') {
+          tiles[index - 1] = label;
+          tiles[index] = 0;
+        }
+        state.tiles = [...tiles];
+      }
+    }
   },
   actions: {
-
+    createTiles({ commit, getters }, difficulty) {
+      const tiles = [];
+      const tilesCount = difficulty ** 2 || getters.totalTilesCount;
+      for (let i = 0; i < tilesCount; i += 1) {
+        tiles.push(i);
+      }
+      if (difficulty) {
+        commit('setRandomiseTiles', difficulty);
+        commit('setTiles', tiles.sort(() => Math.random() - 0.5));
+      } else {
+        commit('setTiles', tiles.sort((a, b) => (a===0)-(b===0) || +(a>b) || -(a<b)));
+      }
+    },
+    updateTiles({ commit, getters }, tile) {
+      const { targetTileNeighbors } = getters;
+      commit('setUpdateTiles', { targetTileNeighbors, tile });
+    }
   }
 });
